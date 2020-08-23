1fad7b2aadfca7e995fe1824c6a055b8e306cd7d
Jerem <jeremydumascontact@gmail.com>
2020-08-23 21:20:57 +0200

    [feat] static integration

---
+++ movies-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Header, MovieList, MovieDetails } from './components';
+
+
+function App() {
+  return (
+    <div className="App d-flex flex-column">
+      <Header />
+      <div className="d-flex flex-row flex-fill pt-4 p-2">
+        <MovieList />
+        <MovieDetails />
+      </div>     
+    </div>
+  );
+}
+
+export default App;
