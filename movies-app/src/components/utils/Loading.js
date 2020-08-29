1b76e582d7f39d4a0e1f98c2d6c0c2c014eea5ca
Jerem <jeremydumascontact@gmail.com>
2020-08-29 21:01:17 +0200

    [feat] loading component & export data

---
+++ movies-app/src/components/utils/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div style={ { minHeight: '100vh' } } className="d-flex flex-row justify-content-center align-items-center w-100">
+      <img alt="loading gif" src="https://motiongraphicsphoebe.files.wordpress.com/2018/10/animated-loading-c397-1.gif" />
+    </div>
+  );  
+}
