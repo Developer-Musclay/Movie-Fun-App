2c0caaf99a411bdb4a50cb47b04b14657e660a58
jerem <jeremydumascontact@gmail.com>
2020-10-03 15:46:01 +0200

    [feat] search bar & selection language

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Header, MovieList, MovieDetails, Loading } from './components';
-import apiMovie from './conf/api.movie';
+import { Header, MovieList, MovieDetails, Loading, SearchBar } from './components';
+import apiMovie, { apiMovieMap }  from './conf/api.movie';
 class App extends Component {
 
   constructor(props) {
@@ -22,18 +22,13 @@ class App extends Component {
     apiMovie.get('/discover/movie')
             .then( response => response.data.results )
             .then( moviesApi => {
-              const movies = moviesApi.map( m => ({
-                img: 'https://image.tmdb.org/t/p/w500' + m.poster_path,
-                title: m.title,
-                details: `${ m.release_date } | ${ m.vote_average }/10 (${ m.vote_count })`,
-                description: m.overview
-              }))
+              const movies = moviesApi.map(apiMovieMap)
               this.updateMovies(movies);
             })
             .catch( err => console.log(err));
   }
 
-  updateMovies(movies) {
+  updateMovies = (movies) => {
     this.setState({
       movies,
       loaded: true
@@ -44,6 +39,7 @@ class App extends Component {
     return (
       <div className="App d-flex flex-column">
         <Header />
+        <SearchBar updateMovies={ this.updateMovies } />
         { this.state.loaded ? (
           <div className="d-flex flex-row flex-fill pt-4 p-2">
             <MovieList movies={ this.state.movies } updateSelectedMovie={ this.updateSelectedMovie }/>
